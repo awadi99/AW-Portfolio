3917a2446387673bb056086225377e81345d8d98
awadi99 <adityawaghmare9990@gmail.com>
2025-11-25 17:01:47 +0530

    setUP

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import { Route, Routes } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import './index.css'
 export default function App() {
   return (
-    <div className='bg-black text-white'>App</div>
-  )
+    <>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
+    </>
+  );
 }
+
+
