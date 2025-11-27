e6e2a44ade81e2af6060a9578693a8199789d311
awadi99 <adityawaghmare9990@gmail.com>
2025-11-28 00:39:33 +0530

    Projects Ui adds

--- Frontend/src/components/Tech.jsx
+++ Frontend/src/components/Tech.jsx
@@ -1,9 +1,20 @@
 import { BallCanvas } from "./canvas/index.js";
 import SectionWapper from "../hoc/SectionWapper.jsx";
 import { technologies } from "../constants/index.js";
-
+import { styles } from "../../style.js";
+import { textVariant } from "../utils/motion.js";
+import {motion} from 'framer-motion'
 function Tech() {
   return (
+    <>
+    <motion.div  variants={textVariant()}>
+    <p className={`${styles.heroSubText} !mt-[90px] !text-[20px] opacity-50 !px-[90px]`}>
+      My Technology
+    </p>
+    <h2 className={`${styles.heroHeadText} !text-[66px] !px-[90px]`}>
+      Tech Stack.
+    </h2>
+  </motion.div>
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28 !mt-20" key={technology.name}>
@@ -11,6 +22,7 @@ function Tech() {
         </div>
       ))}
     </div>
+    </>
   );
 }
 
--- Frontend/src/components/Works.jsx
+++ Frontend/src/components/Works.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "../cart.css";
+import SectionWapper from "../hoc/SectionWapper.jsx";
+import {github} from '../assets/index.js'
+import {motion} from 'framer-motion'
+import { textVariant } from "../utils/motion.js";
+import { styles } from "../../style.js";
 
-export default function Works() {
+function Works() {
+  useEffect(() => {
+    new Swiper(".swiper", {
+      effect: "cards",
+      grabCursor: true,
+      initialSlide: 1,
+      speed: 500,
+      loop: true,
+      cardsEffect: {
+        rotate: true,
+      },
+      navigation: {
+        nextEl: ".next-btn",
+        prevEl: ".prev-btn",
+      },
+    });
+  }, []);
+
   return (
-    <div>Works</div>
-  )
+    <>
+    <motion.div  variants={textVariant()}>
+      <p className={`${styles.heroSubText} !text-[20px] opacity-50 !px-[90px]`}>
+        My work
+      </p>
+      <h2 className={`${styles.heroHeadText} !text-[66px] !px-[90px]`}>
+        Projects.
+      </h2>
+    </motion.div>
+    <section className="movie-section">
+      <div className="content">
+        <div className="info">
+          <p>
+            Join us for a fantastic
+            <span className="movie-night"> movie night </span>
+            filled with popcorn, laughter, and great company! Whether you're a
+            fan of thrilling action, heartwarming dramas, or side-splitting
+            comedies, we've got a film lineup to cater to all tastes.
+          </p>
+          <button className="btn">Join</button>
+        </div>
+
+        {/* SWIPER */}
+        <div className="swiper">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">
+              <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/fc21e481-e28a-41a8-9db3-3b62c1ddc17e" />
+              <div className="overlay">
+                <span>8.1</span>
+                <h2>Wednesday</h2>
+              </div>
+            </div>
+
+            <div className="swiper-slide">
+              <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/b6f5eb64-887c-43b1-aaba-d52a4c59a379" />
+              <div className="overlay">
+                <span>8.5</span>
+                <h2>The Queen's Gambit</h2>
+              </div>
+            </div>
+
+            <div className="swiper-slide">
+              <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e906353b-fde0-4518-9a03-16545c1161bd" />
+              <div className="overlay">
+                <span>9.5</span>
+                <h2>Breaking Bad</h2>
+              </div>
+            </div>
+          </div>
+
+          {/* NAVIGATION BUTTONS */}
+          <button className="prev-btn nav-btn">‹</button>
+          <button className="next-btn nav-btn">›</button>
+        </div>
+      </div>
+
+      {/* BACKGROUND FLOATING SHAPES */}
+      <ul className="circles">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <li key={i}></li>
+        ))}
+      </ul>
+    </section>
+    </>
+  );
 }
+
+export default SectionWapper(Works, "");
