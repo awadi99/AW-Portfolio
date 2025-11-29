3c0aadb3072a9545e7250ed5daeb642a72a150ff
awadi99 <adityawaghmare9990@gmail.com>
2025-11-29 20:57:54 +0530

    new feature

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
       <div className="relative z-0">
         <Contact/>
         <StarsCanvas/>
